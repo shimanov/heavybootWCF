9b4aa9b5e0fdd6d4c338822ba8cf8a7b6eb88092
Dima <d.a.shimanov@gmail.com>
2017-02-19 18:17:01 +0700

    DAL
    Добавил клас для генерации БД (Code first), строку подключения к БД,

---
+++ HeavyBoot/HeavyBoot.DAL/DatabaseConnection/Dbconnection.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+using HeavyBoot.DAL.Entitys;
+
+namespace HeavyBoot.DAL.DatabaseConnection
+{
+    public class Dbconnection : DbContext
+    {
+        public Dbconnection() : base("")
+        {
+            
+        }
+
+        public DbSet<DataTable> DataTables { get; set; }
+    }
+}
---
+++ HeavyBoot/HeavyBoot.DAL/Entitys/DataTable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HeavyBoot.DAL.Entitys
+{
+    public class DataTable
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Key]
+        public string Pcname { get; set; }
+
+        //Время когда я отправляю запрос на клиента
+        public DateTime Dataexport { get; set; }
+
+        //Когда клиент последний раз стучался на сервер
+        public DateTime Dataimport { get; set; }
+
+        public bool IsChecked { get; set; }
+    }
+}
